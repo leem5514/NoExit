bcfd1153d21fadb4c64569496475ca755475dd83
mincastle <kcm0408@naver.com>
2024-07-17 15:18:36 +0900

    NoExit prj first Commit

---
+++ src/main/java/com/E1i3/NoExit/NoExitApplication.java
@@ -0,0 +1,13 @@
+package com.E1i3.NoExit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoExitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoExitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/E1i3/NoExit/NoExitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E1i3.NoExit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoExitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
