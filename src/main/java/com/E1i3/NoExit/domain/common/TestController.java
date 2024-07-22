48a5867e79de13f190ff6ef16bb823d06d8fda28
Mincastle <kcm1042@naver.com>
2024-07-22 17:03:40 +0900

    Topic/TestController

---
+++ src/main/java/com/E1i3/NoExit/domain/common/TestController.java
@@ -0,0 +1,15 @@
+package com.E1i3.NoExit.domain.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String test(){
+        return "실행 테스트 완료.";
+    }
+}
