ff6fa32568e2f84d37882f265632b52284c54030
suhyunpark <psh7492@gmail.com>
2024-07-24 16:02:28 +0900

    feat: 회원 정보 ReqDTO, 메일 인증 ReqDTO

--- src/main/java/com/E1i3/NoExit/domain/member/dto/MemberSaveReqDto.java
+++ src/main/java/com/E1i3/NoExit/domain/member/dto/MemberSaveReqDto.java
@@ -1,2 +1,29 @@
-package com.E1i3.NoExit.domain.member.dto;public class MemberSaveReqDto {
+package com.E1i3.NoExit.domain.member.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSaveReqDto {
+	// 회원가입 정보 기입
+	private String username;
+	private String password;
+	private String email;
+
+	private int age;
+	// private Role role;
+
+	private String phone_number;
+	private String nickname;
+
+	private LocalDateTime createdTime;
+	private LocalDateTime updateTime;
 }
+
