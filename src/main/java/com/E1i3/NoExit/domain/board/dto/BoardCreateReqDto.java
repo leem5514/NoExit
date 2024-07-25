abea38f765c923a99625d7211723e61be5d61606
Mincastle <kcm1042@naver.com>
2024-07-25 22:15:09 +0900

    에러 공유를 위한 브랜치

--- src/main/java/com/E1i3/NoExit/domain/board/dto/BoardCreateReqDto.java
+++ src/main/java/com/E1i3/NoExit/domain/board/dto/BoardCreateReqDto.java
@@ -2,34 +2,29 @@ package com.E1i3.NoExit.domain.board.dto;
 
 import com.E1i3.NoExit.domain.board.domain.Board;
 import com.E1i3.NoExit.domain.board.domain.BoardType;
+import com.E1i3.NoExit.domain.member.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardCreateReqDto {
     private Long memberId; // 작성자 멤버 아이디
-    private String writer; // 작성자 > 이런 거 안 쓰면 에타처럼 익명1 이렇게 되게 해야 할까요
-    private String title; //  제목 > 이런 거 안 쓰면 ㅇ월ㅇ일 ㅇ시ㅇ분에 작성된 글입니다. 디폴트로 되게 만들어야 할까요
+    private String writer; // 작성자 > 익명 처리 등을 위해
+    private String title; // 제목 > 디폴트 값 처리 필요 시 추가
     private String content; // 내용
-//    private String category; // 카테고리
     private String imagePath; // 이미지
     private BoardType boardType; // 게시판 유형 (자유, 전략)
 
-    public Board toEntity() {
+    public Board toEntity(Member member) {
         Board board = Board.builder()
-                .memberId(this.memberId)
+                .member(member) // member를 설정
                 .writer(this.writer)
                 .title(this.title)
                 .content(this.content)
-//                .category(this.category)
                 .imagePath(this.imagePath)
                 .boardType(this.boardType)
                 .build();
@@ -37,4 +32,3 @@ public class BoardCreateReqDto {
         return board;
     }
 }
-
--- src/main/java/com/E1i3/NoExit/domain/member/domain/Member.java
+++ src/main/java/com/E1i3/NoExit/domain/member/domain/Member.java
@@ -12,15 +12,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import com.E1i3.NoExit.domain.member.dto.MemberSaveReqDto;
+import com.E1i3.NoExit.domain.board.domain.Board;
 import com.E1i3.NoExit.domain.member.dto.MemberUpdateDto;
 import com.E1i3.NoExit.domain.reservation.domain.Reservation;
 import com.E1i3.NoExit.domain.review.domain.Review;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,48 +35,45 @@ public class Member {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(length = 50, nullable = false)
 	private String username;
+
 	@Column(length = 50, nullable = false)
 	private String password;
+
 	@Column(length = 100, unique = true)
 	private String email;
 
 	private int point;
 	private int age;
+
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	@Column(length = 255, nullable = false)
 	private String phone_number;
+
 	@Column(length = 100, nullable = false)
 	private String nickname;
 
 	@CreationTimestamp
 	private LocalDateTime createdTime;
+
 	@UpdateTimestamp
 	private LocalDateTime updateTime;
 
-	// @OneToMany(mappedBy = "review", cascade = CascadeType.PERSIST)
-	// private List<Review> reviews;
-	//
-	// @OneToMany(mappedBy = "reservation", cascade = CascadeType.PERSIST)
-	// private List<Reservation> reservations;
-	//
-	// @OneToMany(mappedBy = "storeInfo", cascade = CascadeType.PERSIST)
-	// private List<StoreInfo> storeInfos;
-	//
-	// @OneToOne(mappedBy = "grade", cascade = CascadeType.PERSIST)
-	// private Grade grade;
-	//
-	// @OneToMany(mappedBy = "likes", cascade = CascadeType.PERSIST)
-	// private List<Likes> likes;
-	//
-	// @OneToMany(mappedBy = "board", cascade = CascadeType.PERSIST)
-	// private List<Board> boards;
+	@OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
+	private List<Review> reviews;
 
-	public Member updateMember(MemberUpdateDto dto){
-		// 이메일은 수정x
+	@OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
+	private List<Reservation> reservations;
+
+	@OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
+	private List<Board> boards;
+
+	public Member updateMember(MemberUpdateDto dto) {
+		// 이메일은 수정 x
 		this.username = dto.getUsername();
 		this.password = dto.getPassword();
 		this.age = dto.getAge();
@@ -87,4 +82,3 @@ public class Member {
 		return this;
 	}
 }
-
