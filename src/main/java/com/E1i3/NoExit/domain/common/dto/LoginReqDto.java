f97365c8d334b5d924b4936e579d9be1bc3c4dae
suhyunpark <psh7492@gmail.com>
2024-07-27 04:17:58 +0900

    feat: member, owner 공통 LoginDto 생성

--- src/main/java/com/E1i3/NoExit/domain/common/dto/LoginReqDto.java
+++ src/main/java/com/E1i3/NoExit/domain/common/dto/LoginReqDto.java
@@ -1,2 +1,15 @@
-package com.E1i3.NoExit.domain.common.dto;public class LoginDto {
+package com.E1i3.NoExit.domain.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginReqDto {
+	private String email;
+	private String password;
 }
