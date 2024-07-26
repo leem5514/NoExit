39eef0639fba0ea7914012f5ba0182709f32ae15
suhyunpark <psh7492@gmail.com>
2024-07-27 00:50:39 +0900

    feat: jwt token 로그인 구현

--- src/main/java/com/E1i3/NoExit/domain/member/domain/Member.java
+++ src/main/java/com/E1i3/NoExit/domain/member/domain/Member.java
@@ -14,12 +14,15 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.E1i3.NoExit.domain.board.domain.Board;
+import com.E1i3.NoExit.domain.member.dto.MemberListResDto;
+import com.E1i3.NoExit.domain.member.dto.MemberSaveReqDto;
 import com.E1i3.NoExit.domain.findboard.domain.FindBoard;
 import com.E1i3.NoExit.domain.member.dto.MemberUpdateDto;
 import com.E1i3.NoExit.domain.reservation.domain.Reservation;
 import com.E1i3.NoExit.domain.review.domain.Review;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,7 +43,7 @@ public class Member {
 	@Column(length = 50, nullable = false)
 	private String username;
 
-	@Column(length = 50, nullable = false)
+	@Column(length = 255, nullable = false)
 	private String password;
 
 	@Column(length = 100, unique = true)
@@ -50,13 +53,18 @@ public class Member {
 	private int age;
 
 	@Enumerated(EnumType.STRING)
-	private Role role;
+	@Builder.Default
+	private Role role = Role.USER;
 
 	@Column(length = 255, nullable = false)
-	private String phone_number;
+	private String phoneNumber;
 
 	@Column(length = 100, nullable = false)
-	private String nickname; //카멜케이스..수정..
+	private String nickname;
+
+	@Enumerated(EnumType.STRING)
+	@Builder.Default
+	private DelYN delYN = DelYN.N;
 
 	@CreationTimestamp
 	private LocalDateTime createdTime;
@@ -73,16 +81,51 @@ public class Member {
 	@OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
 	private List<Board> boards;
 
+	public Member updateMember(MemberUpdateDto dto, String encodedPassword) {
 	@OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST) // cascadetype 해야 FK 삭제 가능
 	private List<FindBoard> FindBoards;
 
 	public Member updateMember(MemberUpdateDto dto) {
 		// 이메일은 수정 x
 		this.username = dto.getUsername();
-		this.password = dto.getPassword();
+		this.password =  encodedPassword;
 		this.age = dto.getAge();
-		this.phone_number = dto.getPhone_number();
+		this.phoneNumber = dto.getPhone_number();
 		this.nickname = dto.getNickname();
 		return this;
 	}
+
+	public Member saveMember(MemberSaveReqDto dto,  String encodedPassword) {
+		this.username = dto.getUsername();
+		this.password = encodedPassword;
+		this.age = dto.getAge();
+		this.phoneNumber = dto.getPhoneNumber();
+		this.nickname = dto.getNickname();
+		return this;
+	}
+
+	public Member updateDelYN() {
+		this.delYN = DelYN.Y;
+		return this;
+	}
+
+	public MemberListResDto fromEntity(){
+		return MemberListResDto.builder()
+			.email(this.email)
+			.id(this.id)
+			.nickname(this.nickname)
+			.username(this.username)
+			.build();
+	}
+
+	// public Member toEntity(MemberSaveReqDto dto) {
+	// 	return Member.builder()
+	// 		.username(dto.getUsername())
+	// 		.password(dto.getPassword())
+	// 		.role(dto.getRole())
+	// 		.age(dto.getAge())
+	// 		.phone_number(dto.getPhone_number())
+	// 		.nickname(dto.getNickname())
+	// 		.build();
+	// }
 }
