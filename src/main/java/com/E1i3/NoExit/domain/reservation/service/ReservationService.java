c896233baf836fecc6e399d88f6d658366c613b3
suhyunpark <psh7492@gmail.com>
2024-07-29 17:45:57 +0900

    feat: 로그인, 예약 시스템 redis 데이터베이스 분리

--- src/main/java/com/E1i3/NoExit/domain/reservation/service/ReservationService.java
+++ src/main/java/com/E1i3/NoExit/domain/reservation/service/ReservationService.java
@@ -17,6 +17,7 @@ import com.E1i3.NoExit.domain.reservation.repository.ReservationRepository;
 import lombok.extern.slf4j.XSlf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,7 +50,9 @@ public class ReservationService {
     }
 
     @Autowired
+    @Qualifier("2")
     private RedisTemplate<String, Object> reservationRedisTemplate;
+
 
     private static final String RESERVATION_LOCK_PREFIX = "reservation:lock:";
 
@@ -73,7 +76,6 @@ public class ReservationService {
 
             Reservation reservation = dto.toEntity(member, game);
             reservationRepository.save(reservation);
-
             reservationRedisTemplate.opsForValue().set(reservationKey, "RESERVED", 3, TimeUnit.HOURS); // 3시간 뒤 자동 삭제
 
             return reservation;
