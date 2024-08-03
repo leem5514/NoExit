e0f154beb0511a105b99468abfad84db8eba56ad
leem5514 <ikaros0569@gmail.com>
2024-08-03 17:47:01 +0900

    fix : db에 s3 경로 저장 오류 해결 및 s3 폴더 분리 및 업로드

--- src/main/java/com/E1i3/NoExit/domain/review/service/ReviewService.java
+++ src/main/java/com/E1i3/NoExit/domain/review/service/ReviewService.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
@@ -26,6 +27,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +36,9 @@ import java.util.stream.Collectors;
 public class ReviewService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
+    @Value("${cloud.aws.s3.folder.folderName2}")
+    private String reviewFolder;
 
     private final S3Client s3Client;
     @Autowired
@@ -54,9 +59,8 @@ public class ReviewService {
     /*  */
     @Transactional
     public Review createReview(ReviewSaveDto dto) {
-        MultipartFile image = dto.getReviewImage();
 
-        // 현재 인증된 사용자의 이메일을 가져옴
+        // 리뷰를 작성하기 위한 필요 조건(추가) (예약 state 가 ACCEPT 가 된 조건, 로그인 된 사용자 정보, 예약 일수가 2주가 지나지 않은 사용자)
         String memberEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         Member member = memberRepository.findByEmail(memberEmail)
                 .orElseThrow(() -> new EntityNotFoundException("회원 정보를 찾을 수 없습니다."));
@@ -69,31 +73,29 @@ public class ReviewService {
             throw new IllegalStateException("이미 작성된 리뷰가 있습니다.");
         }
 
-        Review review = dto.toEntity(member, reservation);
-        review = reviewRepository.save(review);
-
+        MultipartFile image = dto.getReviewImage();
+        Review review = null;
         try {
-            if (image != null && !image.isEmpty()) {
-                // 파일을 로컬 디스크에 저장
-                byte[] bytes = image.getBytes();
-                String fileName = review.getId() + "_" + image.getOriginalFilename();
-                Path path = Paths.get("C:/springboot_img", fileName);
-                Files.write(path, bytes);
+            review = reviewRepository.save(dto.toEntity(member, reservation));
+            // 파일을 로컬 디스크에 저장
+            byte[] bytes = image.getBytes();
+            String fileName = review.getId() + "_" + image.getOriginalFilename();
+            Path path = Paths.get("C:/springboot_img", fileName);
 
-                // S3에 파일 업로드
-                PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                        .bucket(bucket)
-                        .key(fileName)
-                        .build();
-                PutObjectResponse putObjectResponse = s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromFile(path));
+            Files.write(path, bytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
 
-                // 업로드된 파일의 S3 URL 가져오기
-                String s3Path = s3Client.utilities().getUrl(a -> a.bucket(bucket).key(fileName)).toExternalForm();
-                review.updateImagePath(s3Path);
+            String s3Key = reviewFolder + fileName;
+            // S3에 파일 업로드
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(s3Key)
+                    .build();
+            PutObjectResponse putObjectResponse = s3Client.putObject(putObjectRequest, RequestBody.fromFile(path));
 
-                // 로컬 파일 삭제
-                Files.delete(path);
-            }
+            // 업로드된 파일의 S3 URL 가져오기
+            String s3Path = s3Client.utilities().getUrl(a -> a.bucket(bucket).key(s3Key)).toExternalForm();
+            review.updateImagePath(s3Path);
+
         } catch (IOException e) {
             throw new RuntimeException("이미지 업로드 실패");
         }
