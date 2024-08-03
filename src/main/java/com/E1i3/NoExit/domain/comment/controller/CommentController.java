31edd7333b0dc3d300a7f4a0f5b2d3ed967d73f2
Mincastle <kcm1042@naver.com>
2024-08-03 14:52:05 +0900

    edit:데이터 타입 수정

--- src/main/java/com/E1i3/NoExit/domain/comment/controller/CommentController.java
+++ src/main/java/com/E1i3/NoExit/domain/comment/controller/CommentController.java
@@ -41,7 +41,7 @@ public class CommentController {
     @PostMapping("/comment/create") // 댓글 생성
     public ResponseEntity<Object> commentCreate(@RequestBody CommentCreateReqDto dto) {
         try {
-            notificationService.notifyComment(dto);
+//            notificationService.notifyComment(dto);
             commentService.commentCreate(dto);
             CommonResDto commonResDto = new CommonResDto(HttpStatus.CREATED, "comment is successfully created", null);
             return new ResponseEntity<>(commonResDto, HttpStatus.CREATED);
