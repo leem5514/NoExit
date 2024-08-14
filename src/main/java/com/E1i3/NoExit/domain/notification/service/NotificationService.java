11ef77b1a221441af24172f8a1cde1e10e6ec6ce
suhyunpark <psh7492@gmail.com>
2024-08-15 02:05:12 +0900

    feat: 알림 기능 구현

--- src/main/java/com/E1i3/NoExit/domain/notification/service/NotificationService.java
+++ src/main/java/com/E1i3/NoExit/domain/notification/service/NotificationService.java
@@ -1,6 +1,10 @@
 package com.E1i3.NoExit.domain.notification.service;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,61 +14,127 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import com.E1i3.NoExit.domain.board.domain.Board;
 import com.E1i3.NoExit.domain.comment.domain.Comment;
 import com.E1i3.NoExit.domain.comment.dto.CommentCreateReqDto;
+import com.E1i3.NoExit.domain.common.domain.DelYN;
+import com.E1i3.NoExit.domain.member.domain.Member;
 import com.E1i3.NoExit.domain.member.domain.Role;
+import com.E1i3.NoExit.domain.member.repository.MemberRepository;
 import com.E1i3.NoExit.domain.member.service.MemberService;
 import com.E1i3.NoExit.domain.notification.controller.NotificationController;
+<<<<<<< Updated upstream
 import com.E1i3.NoExit.domain.notification.dto.UserInfo;
+=======
+import com.E1i3.NoExit.domain.notification.domain.Notification;
+import com.E1i3.NoExit.domain.notification.domain.NotificationType;
+import com.E1i3.NoExit.domain.notification.dto.UserInfo;
+import com.E1i3.NoExit.domain.notification.repository.NotificationRepository;
+>>>>>>> Stashed changes
 import com.E1i3.NoExit.domain.owner.service.OwnerService;
+import com.E1i3.NoExit.domain.reservation.dto.ReservationSaveDto;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class NotificationService {
+	private final Map<UserInfo, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
 
 	private final MemberService memberService;
 	private final OwnerService ownerService;
+	private final NotificationRepository notificationRepository;
 
 	@Autowired
-	public NotificationService(MemberService memberService, OwnerService ownerService) {
+	public NotificationService(MemberService memberService, OwnerService ownerService,
+		NotificationRepository notificationRepository, MemberRepository memberRepository) {
 		this.memberService = memberService;
 		this.ownerService = ownerService;
+		this.notificationRepository = notificationRepository;
 	}
 
-	public SseEmitter subscribe(Role role) {
+	public SseEmitter subscribe(UserInfo userInfo) {
 		SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+<<<<<<< Updated upstream
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		// String email = (role == Role.USER) ? memberService.getEmailFromToken() : ownerService.getEmailFromToken();
 		UserInfo sseEmitterKey = UserInfo.builder().email(email).role(role).build();
+=======
+>>>>>>> Stashed changes
 
 		try {
-			sseEmitter.send(SseEmitter.event().name("connect"));
+			sseEmitter.send(SseEmitter.event().name("connect").data("연결 성공"));
 		} catch (IOException e) {
 			log.info(e.getMessage());
 		}
-		/* 503 Service Unavailable 방지용 dummy event 전송 */
 
-		NotificationController.sseEmitters.put(sseEmitterKey, sseEmitter);
-		sseEmitter.onCompletion(() -> NotificationController.sseEmitters.remove(sseEmitterKey));
-		sseEmitter.onTimeout(() -> NotificationController.sseEmitters.remove(sseEmitterKey));
-		sseEmitter.onError((e) -> NotificationController.sseEmitters.remove(sseEmitterKey));
+		sseEmitters.put(userInfo, sseEmitter);
+		sseEmitter.onCompletion(() -> sseEmitters.remove(userInfo));
+		sseEmitter.onTimeout(() -> sseEmitters.remove(userInfo));
+		sseEmitter.onError((e) -> sseEmitters.remove(userInfo));
 		return sseEmitter;
 	}
 
-	// 	1. 알림 요청시 사용자 -> 점주 예약내역 알림 (ReservationSaveDto)
-	public void notifyResToOwner(){
+	private void notifyUser(UserInfo userInfo, NotificationType type, String message) {
+		SseEmitter sseEmitter = sseEmitters.get(userInfo);
 
+		String receiver = userInfo.getEmail();
+		// if(userInfo.getRole() == Role.USER) {
+		// 	receiver = memberService.getEmailFromToken();
+		// } else if (userInfo.getRole() == Role.OWNER) {
+		// 	receiver = ownerService.getEmailFromToken();
+		// }
+		saveNotification(receiver, type, message);
+
+		if (sseEmitter != null) {
+			try {
+				sseEmitter.send(SseEmitter.event().name(type.name()).data(message));
+				log.info(type.name() + " 이벤트 전송: " + message);
+			} catch (IOException e) {
+				log.info("SSE 전송 오류: " + e.getMessage());
+				sseEmitters.remove(userInfo);
+			}
+		} else {
+			log.info("사용자에게 전송할 SseEmitter가 없습니다: " + userInfo.getEmail());
+		}
 	}
 
-	// 	2. 알림 승인/거절시 점주 -> 사용자 알림 (ReservationStatus, ApprovalStatus) / (ACCEPT, Y) (REJECT, Y)
-	public void notifyResToMember(){
+	private void saveNotification(String receiver, NotificationType type, String message) {
+		notificationRepository.save(
+			Notification.builder()
+				.email(receiver)
+				.type(type)
+				.message(message)
+				.delyn(DelYN.N)
+				.build()
+		);
+	}
 
+	public String getMemberEmail() {
+		return memberService.getEmailFromToken();
+	}
+
+	public String getOwnerEmail() {
+		return ownerService.getEmailFromToken();
+	}
+
+	// 	1. 알림 요청시 사용자 -> 점주 예약내역 알림
+	public void notifyResToOwner(ReservationSaveDto dto) {
+		String senderEmail = memberService.getEmailFromToken();
+		String receiverEmail = ownerService.getEmailFromReservation(dto);
+		String message = senderEmail + "님이 예약을 요청하셨습니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.OWNER).build(), NotificationType.RESERVATION_REQ, message);
+	}
+
+	// 	2. 알림 승인/거절시 점주 -> 사용자 알림
+	public void notifyResToMember(String receiverEmail, String approvalStatus) {
+		String senderEmail = ownerService.getEmailFromToken();
+		String message = senderEmail + "님이 예약을 " + approvalStatus + "하셨습니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.USER).build(), NotificationType.RESERVATION_RES, message);
 	}
 
 	// 	3. 내가 쓴 게시글에 댓글 (작성자, 댓글 내용)
 	public void notifyComment(Board board, CommentCreateReqDto comment) {
 		String senderEmail = memberService.getEmailFromToken();
 		String receiverEmail = board.getMember().getEmail();
+<<<<<<< Updated upstream
 		UserInfo sseEmitterKey = UserInfo.builder().email(receiverEmail).role(Role.USER).build();
 		SseEmitter sseEmitter = NotificationController.sseEmitters.get(sseEmitterKey);
 
@@ -81,14 +151,17 @@ public class NotificationService {
 		}
 		log.info("코멘트 작성 이벤트 보내기 완료");
 		return;
+=======
+		String message = senderEmail + "님이 게시글에 댓글을 남겼습니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.USER).build(), NotificationType.COMMENT, message);
+>>>>>>> Stashed changes
 	}
 
 	// 	4. 내가 쓴 게시글에 추천
 	public void notifyLikeBoard(Board board) {
-		//  board -> board.member.email
-		// board 작성자에게 이벤트 발생
 		String senderEmail = memberService.getEmailFromToken();
 		String receiverEmail = board.getMember().getEmail();
+<<<<<<< Updated upstream
 		UserInfo sseEmitterKey = UserInfo.builder().email(receiverEmail).role(Role.USER).build();
 		SseEmitter sseEmitter = NotificationController.sseEmitters.get(sseEmitterKey);
 
@@ -105,12 +178,17 @@ public class NotificationService {
 			log.info(e.getMessage());
 		}
 		log.info("게시글 추천 이벤트 보내기 완료");
+=======
+		String message = senderEmail + "님이 내 게시글을 추천합니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.USER).build(), NotificationType.BOARD_LIKE, message);
+>>>>>>> Stashed changes
 	}
 
 	// 	5. 내가 쓴 댓글 추천
 	public void notifyLikeComment(Comment comment) {
 		String senderEmail = memberService.getEmailFromToken();
 		String receiverEmail = comment.getMember().getEmail();
+<<<<<<< Updated upstream
 		UserInfo sseEmitterKey = UserInfo.builder().email(receiverEmail).role(Role.USER).build();
 		SseEmitter sseEmitter = NotificationController.sseEmitters.get(sseEmitterKey);
 
@@ -150,6 +228,16 @@ public class NotificationService {
 			log.info(e.getMessage());
 		}
 		log.info("참여인원 제한 이벤트 보내기 완료");
+=======
+		String message = senderEmail + "님이 내 댓글을 추천합니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.USER).build(), NotificationType.COMMENT_LIKE, message);
+	}
+
+	// 	6. findBoard 참여 인원 가득차면
+	public void notifyFullCount(String receiverEmail) {
+		String message = "참여 인원이 가득찼습니다.";
+		notifyUser(UserInfo.builder().email(receiverEmail).role(Role.USER).build(), NotificationType.FULL_COUNT, message);
+>>>>>>> Stashed changes
 	}
 
 }
