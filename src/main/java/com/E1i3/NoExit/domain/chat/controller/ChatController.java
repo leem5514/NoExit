f44fd833e0bedaa77e5c6bd20ada5728705ad818
leem5514 <ikaros0569@gmail.com>
2024-08-16 21:30:04 +0900

    refactor : websock 관련 develop 및 코드 최신화

--- src/main/java/com/E1i3/NoExit/domain/chat/controller/ChatController.java
+++ src/main/java/com/E1i3/NoExit/domain/chat/controller/ChatController.java
@@ -1,28 +1,43 @@
-//package com.E1i3.NoExit.domain.chat.controller;
-//
-//
-//import com.E1i3.NoExit.domain.chat.dto.ChatMessage;
-//import com.E1i3.NoExit.domain.chat.service.RedisMessagePublisher;
-//import com.E1i3.NoExit.domain.chat.service.RedisStreamService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.handler.annotation.SendTo;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class ChatController {
-//
-//    @MessageMapping("/chat.sendMessage")
-//    @SendTo("/topic/public")
-//    public ChatMessage sendMessage(ChatMessage chatMessage) {
-//        return chatMessage;
-//    }
-//
-//    @MessageMapping("/chat.addUser")
-//    @SendTo("/topic/public")
-//    public ChatMessage addUser(ChatMessage chatMessage) {
-//        chatMessage.setContent(chatMessage.getSender() + " joined!");
-//        return chatMessage;
-//    }
-//}
+package com.E1i3.NoExit.domain.chat.controller;
+
+
+import com.E1i3.NoExit.domain.chat.dto.ChatMessage;
+import com.E1i3.NoExit.domain.chat.service.RedisMessagePublisher;
+import com.E1i3.NoExit.domain.chat.service.RedisStreamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final RedisMessagePublisher redisMessagePublisher;
+    private final RedisStreamService redisStreamService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat.sendMessage")
+    public void sendMessage(@Payload ChatMessage chatMessage) {
+        // 메시지를 스트림에 추가
+        redisStreamService.addMessageToStream(chatMessage.getRoomId(), chatMessage.getContent());
+
+        // Redis로 메시지 퍼블리시
+        redisMessagePublisher.publish(chatMessage.getContent());
+
+        // 메시지를 특정 방으로 전송
+        String destination = "/topic/room/" + chatMessage.getRoomId();
+        messagingTemplate.convertAndSend(destination, chatMessage);
+    }
+
+    @MessageMapping("/chat.join")
+    public void joinRoom(@Payload ChatMessage chatMessage) {
+        chatMessage.setContent(chatMessage.getSender() + " joined room " + chatMessage.getRoomId() + "!");
+
+        // 메시지를 특정 방으로 전송
+        String destination = "/topic/room/" + chatMessage.getRoomId();
+        messagingTemplate.convertAndSend(destination, chatMessage);
+    }
+}
