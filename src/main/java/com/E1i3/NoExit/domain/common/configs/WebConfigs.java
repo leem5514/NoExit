9f827825de25ff32a5c94ee229d92ca74105bd30
epiuwiruiw <mandoo2276@naver.com>
2024-08-27 10:16:21 +0900

    feat/board

--- src/main/java/com/E1i3/NoExit/domain/common/configs/WebConfigs.java
+++ src/main/java/com/E1i3/NoExit/domain/common/configs/WebConfigs.java
@@ -19,7 +19,7 @@ public class WebConfigs implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:8083")  // 허용할 클라이언트 도메인
+				.allowedOrigins("http://localhost:8082")  // 허용할 클라이언트 도메인
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 				.allowedHeaders("*")
 				.allowCredentials(true);  // 자격 증명을 허용
